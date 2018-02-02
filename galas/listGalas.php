fc055826cb1ec7f37b9c77d7dd90c23468b26958
Chris Heppell <clheppell1@sheffield.ac.uk>
2018-02-02 16:37:02 +0000

    Statistics using Google Charts
    For Gala Entries

--- galas/listGalas.php
+++ galas/listGalas.php
@@ -10,6 +10,95 @@ if ($access == "Parent") {
 }
 else {
 	$content .= "<p><a href=\"addgala\" class=\"btn btn-success\">Add a gala</a></p>";
+	/* Stats Section */
+	$swimsCountArray = [];
+	$strokesCountArray = [0, 0, 0, 0, 0];
+	$strokesCountTextArray = ["Freestyle", "Breaststroke", "Butterfly", "Backstroke", "Individual Medley"];
+	$swimsArray = ['50Free','100Free','200Free','400Free','800Free','1500Free','50Breast','100Breast','200Breast','50Fly','100Fly','200Fly','50Back','100Back','200Back','100IM','150IM','200IM','400IM',];
+	$strokesArray = ['0','0','0','0','0','0','1','1','1','2','2','2','3','3','3','4','4','4','4',];
+	$swimsTextArray = ['50 Free','100 Free','200 Free','400 Free','800 Free','1500 Free','50 Breast','100 Breast','200 Breast','50 Fly','100 Fly','200 Fly','50 Back','100 Back','200 Back','100 IM','150 IM','200 IM','400 IM',];
+	$counter = 0;
+	for ($i=0; $i<sizeof($swimsArray); $i++) {
+		$col = $swimsArray[$i];
+		$sql = "SELECT `$col` FROM `galaEntries` WHERE `$col` = '1'";
+		$result = mysqli_query($link, $sql);
+		$count = mysqli_num_rows($result);
+		$swimsCountArray[$i] = $count;
+		$strokesCountArray[$strokesArray[$i]] += $count;
+		$counter += $count;
+	}
+		if ($counter>0) {
+		$content .= "<script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>
+		    <script type=\"text/javascript\">
+		      google.charts.load('current', {'packages':['corechart']});
+
+		      google.charts.setOnLoadCallback(drawPieChart);
+					google.charts.setOnLoadCallback(drawBarChart);
+
+		      function drawPieChart() {
+
+		        var data = google.visualization.arrayToDataTable([
+		          ['Stroke', 'Total Number of Entries'],";
+							for ($i=0; $i<sizeof($strokesCountArray); $i++) {
+		          	$content .= "['" . $strokesCountTextArray[$i] . "', " . $strokesCountArray[$i] . "],";
+							}
+		        $content .= "]);
+
+		        var options = {
+		          title: 'Gala Entries by Stroke',
+							fontName: 'Open Sans',
+							backgroundColor: {
+								fill:'transparent'
+							},
+							chartArea: {
+								left: '0',
+								right: '0',
+							}
+		        };
+
+		        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+		        chart.draw(data, options);
+		      }
+					function drawBarChart() {
+
+		        var data = google.visualization.arrayToDataTable([
+		          ['Stroke', 'Total Number of Entries'],";
+							for ($i=0; $i<sizeof($swimsArray); $i++) {
+								if ($swimsCountArray[$i] > 0) {
+		          		$content .= "['" . $swimsTextArray[$i] . "', " . $swimsCountArray[$i] . "],";
+								}
+							}
+		        $content .= "]);
+
+		        var options = {
+		          title: 'Gala Entries by Event',
+							fontName: 'Open Sans',
+							backgroundColor: {
+								fill:'transparent'
+							},
+							chartArea: {
+								left: '0',
+								right: '0',
+							},
+							backgroundColor: {
+								fill:'transparent'
+							},
+							legend: {
+								position: 'none',
+							}
+		        };
+
+		        var chart = new google.visualization.ColumnChart(document.getElementById('barchart'));
+
+		        chart.draw(data, options);
+		      }
+		    </script>
+				<h2>Statistics <small>From all entries</small></h2>
+		    <div id=\"piechart\"></div>
+				<div id=\"barchart\"></div>
+		";
+	}
 }
 $content .= "<h2>Galas closed for entries</h2>";
 $content .= closedGalas($link, false);
--- swimmers/parentSingleSwimmer.php
+++ swimmers/parentSingleSwimmer.php
@@ -118,7 +118,7 @@ if ($swimmersSecurityCheck['UserID'] == $userID && $resultSecurityCheck) {
   $resultSwim = mysqli_query($link, $sqlSwim);
   $rowSwim = mysqli_fetch_array($resultSwim, MYSQLI_ASSOC);
   $title = $swimmersSecurityCheck['MForename'] . " " . $swimmersSecurityCheck['MSurname'];
-  $content = "<div class=\"row\"><div class=\"col col-md-8\">";
+  $content = "<div class=\"row\"><div class=\"col col-md-6\">";
   if ($update) {
   $content .= '<div class="alert alert-success">
     <strong>We have updated</strong>
@@ -214,7 +214,98 @@ if ($swimmersSecurityCheck['UserID'] == $userID && $resultSecurityCheck) {
       </div>
     </div>";
   $content .= "<button type=\"submit\" class=\"btn btn-success mb-3\">Update</button>";
-  $content .= "</div><div class=\"col-md-4\">";
+  $content .= "</div><div class=\"col-md-6\">";
+  /* Stats Section */
+  $swimsCountArray = [];
+  $strokesCountArray = [0, 0, 0, 0, 0];
+  $strokesCountTextArray = ["Freestyle", "Breaststroke", "Butterfly", "Backstroke", "Individual Medley"];
+  $swimsArray = ['50Free','100Free','200Free','400Free','800Free','1500Free','50Breast','100Breast','200Breast','50Fly','100Fly','200Fly','50Back','100Back','200Back','100IM','150IM','200IM','400IM',];
+  $strokesArray = ['0','0','0','0','0','0','1','1','1','2','2','2','3','3','3','4','4','4','4',];
+  $swimsTextArray = ['50 Free','100 Free','200 Free','400 Free','800 Free','1500 Free','50 Breast','100 Breast','200 Breast','50 Fly','100 Fly','200 Fly','50 Back','100 Back','200 Back','100 IM','150 IM','200 IM','400 IM',];
+  $counter = 0;
+  for ($i=0; $i<sizeof($swimsArray); $i++) {
+  	$col = $swimsArray[$i];
+  	$sql = "SELECT `$col` FROM `galaEntries` WHERE `MemberID` = '$id' AND `$col` = '1'";
+  	$result = mysqli_query($link, $sql);
+  	$count = mysqli_num_rows($result);
+  	$swimsCountArray[$i] = $count;
+  	$strokesCountArray[$strokesArray[$i]] += $count;
+  	$counter += $count;
+  }
+  	if ($counter>0) {
+  	$content .= "<script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>
+  	    <script type=\"text/javascript\">
+  	      google.charts.load('current', {'packages':['corechart']});
+
+  	      google.charts.setOnLoadCallback(drawPieChart);
+  				google.charts.setOnLoadCallback(drawBarChart);
+
+  	      function drawPieChart() {
+
+  	        var data = google.visualization.arrayToDataTable([
+  	          ['Stroke', 'Total Number of Entries'],";
+  						for ($i=0; $i<sizeof($strokesCountArray); $i++) {
+  	          	$content .= "['" . $strokesCountTextArray[$i] . "', " . $strokesCountArray[$i] . "],";
+  						}
+  	        $content .= "]);
+
+  	        var options = {
+  	          title: 'Gala Entries by Stroke',
+  						fontName: 'Open Sans',
+  						backgroundColor: {
+  							fill:'transparent'
+  						},
+  						chartArea: {
+  							left: '0',
+  							right: '0',
+  						}
+  	        };
+
+  	        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+  	        chart.draw(data, options);
+  	      }
+  				function drawBarChart() {
+
+  	        var data = google.visualization.arrayToDataTable([
+  	          ['Stroke', 'Total Number of Entries'],";
+  						for ($i=0; $i<sizeof($swimsArray); $i++) {
+  							if ($swimsCountArray[$i] > 0) {
+  	          		$content .= "['" . $swimsTextArray[$i] . "', " . $swimsCountArray[$i] . "],";
+  							}
+  						}
+  	        $content .= "]);
+
+  	        var options = {
+  	          title: 'Gala Entries by Event',
+  						fontName: 'Open Sans',
+  						backgroundColor: {
+  							fill:'transparent'
+  						},
+  						chartArea: {
+  							left: '0',
+  							right: '0',
+  						},
+  						backgroundColor: {
+  							fill:'transparent'
+  						},
+  						legend: {
+  							position: 'none',
+  						}
+  	        };
+
+  	        var chart = new google.visualization.ColumnChart(document.getElementById('barchart'));
+
+  	        chart.draw(data, options);
+  	      }
+  	    </script>
+        <div class=\"cell\">
+  			<h2>Gala Statistics</h2>
+  	    <div id=\"piechart\"></div>
+  			<div id=\"barchart\"></div>
+        </div>
+  	";
+  }
   $content .= "<div class=\"cell\"><h2>Squad Information</h2><ul class=\"mb-0\"><li>Squad: " . $rowSwim['SquadName'] . "</li><li>Monthly Fee: &pound;" . $rowSwim['SquadFee'] . "</li>";
   if ($rowSwim['SquadTimetable'] != "") {
     $content .= "<li><a href=\"" . $rowSwim['SquadTimetable'] . "\">Squad Timetable</a></li>";
