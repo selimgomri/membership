a6b4af85e8aaf8052ce60eb94beaf48c5607f293
Chris Heppell <clheppell1@sheffield.ac.uk>
2018-02-03 01:30:22 +0000

    New stats

--- squads/SquadIndividual.php
+++ squads/SquadIndividual.php
@@ -74,6 +74,10 @@ $title = $pagetitle = $row['SquadName'] . " Squad";
 $content = "<form method=\"post\">";
 if ($access == "Admin") {
 $content .= "
+<div class=\"row\"><div class=\"col-md-6\">
+<div class=\"my-3 p-3 bg-white rounded box-shadow\">
+<h2>Details</h2>
+<p class=\"lead border-bottom border-gray pb-2 mb-0\">View or edit the squad details</p>
 <div class=\"form-group\">
   <label for=\"squadName\">Squad Name</label>
   <input type=\"text\" class=\"form-control\" id=\"squadName\" name=\"squadName\" placeholder=\"Enter Squad Name\" value=\"" . $row['SquadName'] . "\">
@@ -107,10 +111,67 @@ $content .= "
     <small id=\"squadDeleteDangerHelp\" class=\"form-text\">Enter the key in quotes above and press submit. This will delete this squad.</small>
   </div>
 </div>
-<p><button class=\"btn btn-success\" type=\"submit\">Update</button></p>";
+<p><button class=\"btn btn-success\" type=\"submit\">Update</button></p></div></div>
+<div class=\"col-md-6\">";
+
+$sql = "SELECT `Gender` FROM `members` WHERE `SquadID` = '$id' AND `Gender` = 'Male';";
+$result = mysqli_query($link, $sql);
+$male = mysqli_num_rows($result);
+$sql = "SELECT `Gender` FROM `members` WHERE `SquadID` = '$id' AND `Gender` = 'Female';";
+$result = mysqli_query($link, $sql);
+$female = mysqli_num_rows($result);
+
+  if ($male+$female>0) {
+  $content .= "<script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>
+      <script type=\"text/javascript\">
+        google.charts.load('current', {'packages':['corechart']});
+
+        google.charts.setOnLoadCallback(drawPieChart);
+        function drawPieChart() {
+
+          var data = google.visualization.arrayToDataTable([
+            ['Gender', 'Number of Members'],
+            ['Male', " . $male . "],
+            ['Female', " . $female . "],
+          ]);
+
+          var options = {
+            title: 'Squad Gender Split',
+            fontName: 'Open Sans',
+            backgroundColor: {
+              fill:'transparent'
+            },
+            chartArea: {
+              left: '0',
+              right: '0',
+            },
+            backgroundColor: {
+              fill:'transparent'
+            },
+            legend: {
+              position: 'none',
+            }
+          };
+
+          var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+          chart.draw(data, options);
+        }
+      </script>
+      <div class=\"my-3 p-3 bg-white rounded box-shadow\">
+      <h2>Statistics</h2>
+      <p class=\"lead border-bottom border-gray pb-2 mb-0\">These statistics are gathered from our system</p>
+      <div class=\"chart\" id=\"piechart\"></div>
+      </div></div></div>
+  ";
+
+  }
 }
 else {
-  $content .= "<ul>";
+  $content .= "<div class=\"row\"><div class=\"col-md-6\">
+  <div class=\"my-3 p-3 bg-white rounded box-shadow\">
+  <h2 class=\"border-bottom border-gray pb-2 mb-0\">Squad Details</h2>
+  <ul>";
   if ($row['SquadFee'] > 0) {
     $content .= "<li>Squad Fee: &pound;" . $row['SquadFee'] . "</li>";
   }
@@ -121,7 +182,59 @@ else {
     <li>Squad Coach: " . $row['SquadCoach'] . "</li>
     <li><a href=\"" . $row['SquadTimetable'] . "\">Squad Timetable</a></li>
     <li><a href=\"" . $row['SquadCoC'] . "\">Squad Code of Conduct</a></li>
-  </ul>";
+  </ul></div></div>";
+  $sql = "SELECT `Gender` FROM `members` WHERE `SquadID` = '$id' AND `Gender` = 'Male';";
+  $result = mysqli_query($link, $sql);
+  $male = mysqli_num_rows($result);
+  $sql = "SELECT `Gender` FROM `members` WHERE `SquadID` = '$id' AND `Gender` = 'Female';";
+  $result = mysqli_query($link, $sql);
+  $female = mysqli_num_rows($result);
+
+    if ($male+$female>0) {
+    $content .= "<script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>
+        <script type=\"text/javascript\">
+          google.charts.load('current', {'packages':['corechart']});
+
+          google.charts.setOnLoadCallback(drawPieChart);
+          function drawPieChart() {
+
+            var data = google.visualization.arrayToDataTable([
+              ['Gender', 'Number of Members'],
+              ['Male', " . $male . "],
+              ['Female', " . $female . "],
+            ]);
+
+            var options = {
+              title: 'Squad Gender Split',
+              fontName: 'Open Sans',
+              backgroundColor: {
+                fill:'transparent'
+              },
+              chartArea: {
+                left: '0',
+                right: '0',
+              },
+              backgroundColor: {
+                fill:'transparent'
+              },
+              legend: {
+                position: 'none',
+              }
+            };
+
+            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+            chart.draw(data, options);
+          }
+        </script>
+        <div class=\"col-md-6\">
+        <div class=\"my-3 p-3 bg-white rounded box-shadow\">
+        <h2>Statistics</h2>
+        <p class=\"lead border-bottom border-gray pb-2 mb-0\">These statistics are gathered from our system</p>
+        <div class=\"chart\" id=\"piechart\"></div>
+        </div></div></div>
+    ";
+  }
 }
 $content .= "</form>";
 
