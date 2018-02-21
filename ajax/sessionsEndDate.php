c982b1797f9ba53edf12972514a7d836a60dc7d7
Chris Heppell <clheppell1@sheffield.ac.uk>
2018-02-21 17:34:04 +0000

    Bringing over

---
+++ ajax/sessionsEndDate.php
@@ -0,0 +1,17 @@
+<?php
+include_once "../database.php";
+$access = $_SESSION['AccessLevel'];
+if ($access == "Committee" || $access == "Admin") {
+	if ((isset($_POST["sessionID"])) && (isset($_POST["sessionEndDate"]))) {
+
+		// get the galaID parameter from POST
+    $id = mysqli_real_escape_string($link, htmlspecialchars($_POST["sessionID"]));
+		$endDate = mysqli_real_escape_string($link, htmlspecialchars($_POST["sessionEndDate"]));
+
+		if ($id != null) {
+			$sql = "UPDATE `sessions` SET `DisplayUntil` = '$endDate' WHERE `SessionID` = '$id'";
+			mysqli_query($link, $sql);
+		}
+	}
+}
+?>
