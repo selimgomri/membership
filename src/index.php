3ae15065e807aaff617f2547d12b6b3e8b006434
Chris Heppell <clheppell@icloud.com>
2018-06-13 23:38:00 +0100

    SQL to sum last months fees if there are any

--- src/index.php
+++ src/index.php
@@ -94,6 +94,34 @@ for ($i = 0; $i < $count; $i++) {
   }
 }
 
+$sql = "SELECT * FROM `paymentMonths` ORDER BY `Date` DESC LIMIT 1;";
+$result = mysqli_query($link, $sql);
+if (mysqli_num_rows($result) > 0) {
+  $row = mysqli_fetch_array($result);
+  if ($row['MonthStart'] != date("Y-m")) {
+    $ms = date("Y-m");
+    $date = $ms . "-01";
+    $sql = "INSERT INTO `paymentMonths` (`MonthStart`, `Date`) VALUES ('$ms', '$date');";
+    mysqli_query($link, $sql);
+  }
+}
+
+$date = date("Y-m") . "-01";
+
+// If pending payments for last month
+$sql = "SELECT DISTINCT `UserID` FROM `paymentsPending` WHERE `Status` = 'Pending' AND `Date` < '$date';";
+$result = mysqli_query($link, $sql);
+$count = mysqli_num_rows($result);
+for ($i = 0; $i < $count; $i++) {
+  $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+  $sumUID = $row['UserID'];
+  $sql = "SELECT SUM(`Amount`) FROM `paymentsPending` WHERE `UserID` = '$sumUID' AND `Status` = 'Pending' AND `Date` < '$date';";
+  $result = mysqli_query($link, $sql);
+  $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+  // print_r($row);
+}
+//echo date("Y-m", strtotime("first day of previous month"));
+
 require_once "database.php";
 
 $app            = System\App::instance();
