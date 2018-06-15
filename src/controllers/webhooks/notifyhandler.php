e06ef765ce6b1b8ceea3bb59830e797b1e3d7a29
Chris Heppell <clheppell@icloud.com>
2018-06-15 01:07:11 +0100

    Working Notify

--- src/controllers/webhooks/notifyhandler.php
+++ src/controllers/webhooks/notifyhandler.php
@@ -3,17 +3,22 @@
 ignore_user_abort(true);
 set_time_limit(0);
 
-$sql = "SELECT * FROM `notify` INNER JOIN `users` ON notify.UserID = users.UserID WHERE `Status` = 'Queued' LIMIT 15;";
+$sql = "SELECT * FROM `notify` INNER JOIN `users` ON notify.UserID = users.UserID WHERE `Status` = 'Queued' LIMIT 4;";
 $result = mysqli_query($link, $sql);
 
 for ($i = 0; $i < mysqli_num_rows($result); $i++) {
 	$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-	$to = $row['Forename'] . " " . $row['Surname'] . "<web@chesterlestreetasc.co.uk>";
-	$subject = $row['Subject'];
-	$message = "<p>Dear " . $row['Forename'] . " " . $row['Surname'] . "</p>" . $row['Message'];
-	notifySend($to, $subject, $message);
-
 	$emailid = $row['EmailID'];
-	$sql = "UPDATE `notify` SET `Status` = 'Sent' WHERE `EmailID` = '$emailid';";
-	mysqli_query($link, $sql);
+	if ($row['EmailComms'] == 1) {
+		$to = $row['Forename'] . " " . $row['Surname'] . "<" . $row['EmailAddress'] . ">";
+		$subject = $row['Subject'];
+		$message = "<p>Dear " . $row['Forename'] . " " . $row['Surname'] . ",</p>" . $row['Message'];
+		notifySend($to, $subject, $message);
+
+		$sql = "UPDATE `notify` SET `Status` = 'Sent' WHERE `EmailID` = '$emailid';";
+		mysqli_query($link, $sql);
+	} else {
+		$sql = "UPDATE `notify` SET `Status` = 'No_Sub' WHERE `EmailID` = '$emailid';";
+		mysqli_query($link, $sql);
+	}
 }
