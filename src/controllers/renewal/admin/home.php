a7a982a5d6dd2d4c284d854748cfdd9b52f83290
Chris Heppell <clheppell@icloud.com>
2018-07-09 17:58:27 +0100

    Finished renewal admin views

--- src/controllers/renewal/admin/home.php
+++ src/controllers/renewal/admin/home.php
@@ -1,5 +1,8 @@
 <?php
 
+$sql = "SELECT * FROM `renewals` ORDER BY `EndDate` DESC LIMIT 5;";
+$renewals = mysqli_query($link, $sql);
+
 $sql = "SELECT * FROM `renewals` WHERE `StartDate` <= CURDATE() <= `EndDate`;";
 $result = mysqli_query($link, $sql);
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
@@ -23,10 +26,6 @@ include BASE_PATH . "views/header.php";
 		<p>
 			We now charge fees by Direct Debit.
 		</p>
-		<p>
-			<a class="btn btn-success" href="<? echo autoUrl("renewal/go"); ?>">Get
-			Started</a>
-		</p>
 	<? } else { ?>
 		<h1>Membership Renewal for <? echo $row['Year']; ?></h1>
 		<p class="lead">Welcome to the Membership Renewal System</p>
@@ -39,6 +38,19 @@ include BASE_PATH . "views/header.php";
 			We'll let you know when this starts
 		</div>
 	<? } ?>
-</div>
+	<h2>Previous and Current Renewals</h2>
+	<ol>
+		<? for ($i = 0; $i < mysqli_num_rows($renewals); $i++) {
+			$renewalArray = mysqli_fetch_array($renewals, MYSQLI_ASSOC);
+			?>
+			<li>
+				<a href="<? echo autoUrl("renewal/" . $renewalArray['ID']); ?>">
+					<? echo $renewalArray['Name']; ?> (<? echo date("j F Y",
+					strtotime($renewalArray['StartDate'])); ?> - <? echo date("j F Y",
+					strtotime($renewalArray['EndDate'])); ?>)
+			</li><?
+		} ?>
+	</ol>
+ </div>
 
 <?php include BASE_PATH . "views/footer.php";
--- src/controllers/renewal/admin/list.php
+++ src/controllers/renewal/admin/list.php
@@ -2,14 +2,23 @@
 
 $id = mysqli_real_escape_string($link, $id);
 
+$sql = "SELECT * FROM `renewals` WHERE `ID` = '$id';";
+$result = mysqli_query($link, $sql);
+
+if (mysqli_num_rows($result) == 0) {
+	halt(404);
+}
+
+$renewalArray = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
 $sql = "SELECT `MForename`, `MSurname`, `Forename`, `Surname`, `ASANumber`,
-`payments`.`Status`, `RenewalID` FROM ((((`members` LEFT JOIN `renewalMembers`
+`payments`.`Status`, `RenewalID` FROM ((((`renewalMembers` RIGHT JOIN `members`
 ON members.MemberID = renewalMembers.MemberID) LEFT JOIN `users` ON
 members.UserID = users.UserID) LEFT JOIN `paymentsPending` ON
 renewalMembers.PaymentID = paymentsPending.PaymentID) LEFT JOIN `payments` ON
 payments.PMkey = paymentsPending.PMkey) WHERE `renewalMembers`.`RenewalID` =
-'$id' OR `renewalMembers`.`RenewalID` IS NULL ORDER BY `MForename` ASC,
-`MSurname` ASC;";
+'$id' OR `renewalMembers`.`RenewalID` IS NULL OR `renewalMembers`.`RenewalID` IS
+NOT NULL ORDER BY `MForename` ASC, `MSurname` ASC;";
 $result = mysqli_query($link, $sql);
 
 if (mysqli_num_rows($result) == 0) {
@@ -22,6 +31,11 @@ include BASE_PATH . "views/header.php";
 ?>
 
 <div class="container">
+	<h1><? echo $renewalArray['Name']; ?> Status</h1>
+	<p class="lead">
+		This is the current status for this membership renewal which started on <? echo date("l j F Y", strtotime($renewalArray['StartDate'])); ?> and finishes on <? echo date("l j F Y", strtotime($renewalArray['EndDate'])); ?>
+	</p>
+
 	<table class="table">
 		<thead class="thead-light">
 			<tr>
@@ -42,7 +56,8 @@ include BASE_PATH . "views/header.php";
 		<tbody>
 			<? for ($i = 0; $i < mysqli_num_rows($result); $i++) {
 				$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-				if ($row['RenewalID'] == "" || $row['Status'] == "failed" || $row['Status'] == "charged_back") {
+				if ($row['RenewalID'] == "" || $row['RenewalID'] != $id ||
+				$row['Status'] == "failed" || $row['Status'] == "charged_back") {
 					?><tr class="table-danger"><?
 				} else if ($row['Status'] == "paid_out" || $row['Status'] == "confirmed") {
 					?><tr class="table-success"><?
@@ -62,7 +77,13 @@ include BASE_PATH . "views/header.php";
 						</span>
 					</td>
 					<td>
-						<? echo paymentStatusString($row['Status']); ?>
+						<? if ($row['RenewalID'] == "" || $row['RenewalID'] != $id) {
+							?>No Renewal Exists<?
+						} else if ($row['Status'] == "") {
+							?>Payment not yet processed<?
+						} else {
+							echo paymentStatusString($row['Status']);
+						} ?>
 					</td>
 				</tr>
 				<?
