8e20958c3726093e969ebbed083b97c377cc195d
Chris Heppell <clheppell@icloud.com>
2019-04-21 18:27:28 +0100

    Redesign user info page

--- src/controllers/users/userSettingsAjax.php
+++ src/controllers/users/userSettingsAjax.php
@@ -1,15 +1,17 @@
 <?php
-$id = mysqli_real_escape_string($link, $id);
-$newAccess = mysqli_real_escape_string($link, $_POST['accountType']);
+
+global $db;
+
+$newAccess = $_POST['accountType'];
 
 if ($newAccess != "Admin" && $newAccess != "Committee" && $newAccess != "Coach" && $newAccess != "Galas" && $newAccess != "Parent") {
 	$newAccess = "Parent";
 }
 
-$sql = "UPDATE `users` SET `AccessLevel` = '$newAccess' WHERE `UserID` = '$id';";
-if (mysqli_query($link, $sql)) {
-	?><span class="text-success pt-3">Updated Account Type Successfully</span><?php
-} else {
-	?><span class="text-danger mt-2">Failed to update account type</span><?php
+$query = $db->prepare("UPDATE `users` SET AccessLevel = ? WHERE UserID = ?");
+try {
+  $query->execute([$newAccess, $id]);
+  ?><span class="text-success pt-3">Successfully updated account type to <?=strtolower($newAccess)?></span><?php
+} catch (Exception $e) {
+  ?><span class="text-danger mt-2">Failed to update account type</span><?php
 }
-?>
