e0b96ce92ab4bf51336ae0414f99107867b07213
Chris Heppell <clheppell@icloud.com>
2019-05-11 17:14:25 +0100

    Fix syntax error

--- src/public/js/posts/PostEditor.js
+++ src/public/js/posts/PostEditor.js
@@ -1,4 +1,4 @@
 document.querySelector("#content").oninput = function() {
 	var scrollHeight = document.querySelector("#content").scrollHeight
   document.querySelector("#content").style.height = (scrollHeight + 20) + "px";
-});
+};
