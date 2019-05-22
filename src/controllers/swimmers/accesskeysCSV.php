45aa1ee5dda6d3b8d90d0dcab3c859f9476afad0
Chris Heppell <chris.heppell@chesterlestreetasc.co.uk>
2019-05-22 22:15:24 +0000

    Access key updates

--- src/controllers/swimmers/accesskeysCSV.php
+++ src/controllers/swimmers/accesskeysCSV.php
@@ -1,4 +1,7 @@
 <?php
+
+global $db;
+
 // output headers so that the file is downloaded rather than displayed
 header('Content-Type: text/csv; charset=utf-8');
 header('Content-Disposition: attachment; filename=accessKeys.csv');
@@ -13,7 +16,7 @@ fputcsv($output, array('Forename', 'Surname', 'Squad' , 'ASA Number' , 'Access K
 $swimmers = $db->query("SELECT members.MForename, members.MSurname, squads.SquadName, members.ASANumber, members.AccessKey FROM (members INNER JOIN squads ON members.SquadID = squads.SquadID) ORDER BY `members`.`MForename` , `members`.`MSurname` ASC");
 
 // loop over the rows, outputting them
-while ($row = $swimmers->fetch(PDO::FETCH_ASSOC) {
+while ($row = $swimmers->fetch(PDO::FETCH_ASSOC)) {
   fputcsv($output, $row);
 }
 ?>
