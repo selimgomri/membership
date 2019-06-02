3452ed39ff71643f0cbe2967315b4b40c452d924
Chris Heppell <clheppell@icloud.com>
2019-06-02 11:21:01 +0100

    Fix one more I missed

--- src/views/chester/GlobalHead.php
+++ src/views/chester/GlobalHead.php
@@ -77,21 +77,21 @@ Chester-le-Street ASC is a non profit unincorporated association.
     <!-- For Chrome for Android: -->
     <link rel="icon" sizes="192x192" href="<?=autoUrl("public/img/touchicons/touch-icon-192x192.png")?>">
     <!-- For iPhone 6 Plus with @3× display: -->
-    <link rel="apple-touch-icon-precomposed" sizes="180x180" href="<?=autoUrl("public/img/touchicons/touchicons/apple-touch-icon-180x180-precomposed.png")?>">
+    <link rel="apple-touch-icon-precomposed" sizes="180x180" href="<?=autoUrl("public/img/touchicons/apple-touch-icon-180x180-precomposed.png")?>">
     <!-- For iPad with @2× display running iOS ≥ 7: -->
-    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?=autoUrl("public/img/touchicons/touchicons/apple-touch-icon-152x152-precomposed.png")?>">
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?=autoUrl("public/img/touchicons/apple-touch-icon-152x152-precomposed.png")?>">
     <!-- For iPad with @2× display running iOS ≤ 6: -->
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?=autoUrl("public/img/touchicons/touchicons/apple-touch-icon-144x144-precomposed.png")?>">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?=autoUrl("public/img/touchicons/apple-touch-icon-144x144-precomposed.png")?>">
     <!-- For iPhone with @2× display running iOS ≥ 7: -->
-    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?=autoUrl("public/img/touchicons/touchicons/apple-touch-icon-120x120-precomposed.png")?>">
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?=autoUrl("public/img/touchicons/apple-touch-icon-120x120-precomposed.png")?>">
     <!-- For iPhone with @2× display running iOS ≤ 6: -->
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?=autoUrl("public/img/touchicons/touchicons/apple-touch-icon-114x114-precomposed.png")?>">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?=autoUrl("public/img/touchicons/apple-touch-icon-114x114-precomposed.png")?>">
     <!-- For the iPad mini and the first- and second-generation iPad (@1× display) on iOS ≥ 7: -->
-    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="<?=autoUrl("public/img/touchicons/touchicons/apple-touch-icon-76x76-precomposed.png")?>">
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="<?=autoUrl("public/img/touchicons/apple-touch-icon-76x76-precomposed.png")?>">
     <!-- For the iPad mini and the first- and second-generation iPad (@1× display) on iOS ≤ 6: -->
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?=autoUrl("public/img/touchicons/touchicons/apple-touch-icon-72x72-precomposed.png")?>">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?=autoUrl("public/img/touchicons/apple-touch-icon-72x72-precomposed.png")?>">
     <!-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
-    <link rel="apple-touch-icon-precomposed" href="<?=autoUrl("public/img/touchicons/touchicons/apple-touch-icon-precomposed.png")?>"><!-- 57×57px -->
+    <link rel="apple-touch-icon-precomposed" href="<?=autoUrl("public/img/touchicons/apple-touch-icon-precomposed.png")?>"><!-- 57×57px -->
     <link rel="mask-icon" href="<?=autoUrl("public/img/touchicons/icon-mask.svg")?>" color="#bd0000">
     <script src='https://www.google.com/recaptcha/api.js'></script>
     <script src="<?=autoUrl("public/js/tinymce/tinymce.min.js")?>"></script>
