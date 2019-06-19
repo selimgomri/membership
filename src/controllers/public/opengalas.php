6691e6f4b496ac7a0ca841a2751c9f7083fa1e50
Chris Heppell <chris.heppell@chesterlestreetasc.co.uk>
2019-06-19 21:58:58 +0000

    OpenGalas change

--- src/controllers/public/opengalas.php
+++ src/controllers/public/opengalas.php
@@ -1,11 +1,62 @@
+<?php
+
+global $db;
+
+$galas = $db->query("SELECT GalaID, GalaName, ClosingDate, GalaDate, GalaVenue, CourseLength FROM galas WHERE ClosingDate >= CURDATE() ORDER BY GalaDate ASC");
+$gala = $galas->fetch(PDO::FETCH_ASSOC);
+$entriesOpen = false;
+
+?>
+
 <div class="cell">
 	<h2>
-		Galas open for entries
+		Open galas
 	</h2>
-	<?php echo upcomingGalas($link, false); ?>
-	<p class="mb-0">
-		Enter via <a target="_blank"
-		href="https://www.chesterlestreetasc.co.uk/competitions/enteracompetition/">My
-		Account</a>
+	<?php if ($gala != null) { ?>
+	<div class="news-grid my-3">
+		<?php do {
+			$now = new DateTime();
+			$closingDate = new DateTime($gala['ClosingDate']);
+			$endDate = new DateTime($gala['GalaDate']);
+
+			if ($now <= $closingDate) {
+				$entriesOpen = true;
+			}
+
+			?>
+			<a href="<?=autoUrl("galas/" . $gala['GalaID'])?>">
+				<div>
+					<span class="title mb-0 justify-content-between align-items-start">
+						<span><?=htmlspecialchars($gala['GalaName'])?></span>
+						<?php if ($now <= $closingDate) { $entriesOpen = true;?><span class="ml-2 badge badge-success">ENTRIES OPEN</span><?php } ?>
+					</span>
+					<span class="d-flex mb-3"><?=htmlspecialchars($gala['GalaVenue'])?></span>
+				</div>
+				<?php if ($now <= $closingDate) { ?>
+				<span class="category">Entries close on <?=$closingDate->format('j F Y')?></span>
+				<?php } else { ?>
+				<span class="category">Ends on <?=$endDate->format('j F Y')?></span>
+				<?php } ?>
+			</a>
+		<?php } while ($gala = $galas->fetch(PDO::FETCH_ASSOC)); ?>
+	</div>
+	<?php if ($entriesOpen) { ?>
+	<p>
+		<a href="<?=autoUrl("galas/entergala")?>" class="btn btn-success">
+			Enter a gala
+		</a>
 	</p>
+  <p class="mb-0">
+    Enter via My Account
+  </p>
+  <?php } else { ?>
+  <p class="mb-0">
+    There are no galas open for entries at the moment.
+  </p>
+  <?php } ?>
+  <?php } else { ?>
+  <p class="mb-0">
+		There are no galas open for entries at the moment.
+	</p>
+  <?php } ?>
 </div>
