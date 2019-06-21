d9cdc8794afa41b92a4dcb7bcf885cfcf06dc1b4
Chris Heppell <clheppell@icloud.com>
2019-06-21 12:42:12 +0100

    Mods

--- src/controllers/renewal/admin/home.php
+++ src/controllers/renewal/admin/home.php
@@ -1,11 +1,11 @@
 <?php
 
-$sql = "SELECT * FROM `renewals` ORDER BY `EndDate` DESC LIMIT 5;";
-$renewals = mysqli_query($link, $sql);
+global $db;
 
-$sql = "SELECT * FROM `renewals` WHERE `StartDate` <= CURDATE() AND CURDATE() <= `EndDate`;";
-$result = mysqli_query($link, $sql);
-$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+$renewals = $db->query("SELECT ID, `Name`, StartDate, EndDate FROM `renewals` ORDER BY `EndDate` DESC LIMIT 5;");
+
+$getRenewals = $db->query("SELECT * FROM `renewals` WHERE `StartDate` <= CURDATE() AND CURDATE() <= `EndDate`;");
+$row = $getRenewals->fetch(PDO::FETCH_ASSOC);
 
 $use_white_background = true;
 $pagetitle = "Membership Renewal";
@@ -15,7 +15,7 @@ include BASE_PATH . "views/swimmersMenu.php";
 
 <div class="container">
 	<div class="">
-	<?php if (mysqli_num_rows($result) > 0) { ?>
+		<?php if ($row != null) { ?>
 		<h1>Membership Renewal for <?php echo $row['Year']; ?></h1>
 		<p class="lead">Welcome to the Membership Renewal System for <?php echo
 		$row['Year']; ?></p>
@@ -29,7 +29,7 @@ include BASE_PATH . "views/swimmersMenu.php";
 		<p>
 			We now charge fees by Direct Debit.
 		</p>
-	<?php } else { ?>
+		<?php } else { ?>
 		<h1>Membership Renewal System</h1>
 		<p class="lead">Welcome to the Membership Renewal System</p>
 		<p>
@@ -40,29 +40,28 @@ include BASE_PATH . "views/swimmersMenu.php";
 			<strong>There is no open Renewal Period right now</strong> <br>
 			You'll need to add one first
 		</div>
-	<?php } ?>
-	<h2>Previous and Current Renewals</h2>
-	<ol>
-		<?php for ($i = 0; $i < mysqli_num_rows($renewals); $i++) {
-			$renewalArray = mysqli_fetch_array($renewals, MYSQLI_ASSOC);
-			?>
-			<li>
-				<a href="<?php echo autoUrl("renewal/" . $renewalArray['ID']); ?>">
-					<?php echo $renewalArray['Name']; ?> (<?php echo date("j F Y",
-					strtotime($renewalArray['StartDate'])); ?> - <?php echo date("j F Y",
-					strtotime($renewalArray['EndDate'])); ?>)
-				</a>
-			</li><?
-		} ?>
-	</ol>
-	<h2>Add a new Renewal Period</h2>
-	<p class="mb-0">
-		<a href="<?php echo autoUrl("renewal/new"); ?>" class="btn
-		btn-success">
-			Add new Renewal Period
-		</a>
-	</p>
+		<?php } ?>
+		<h2>Previous and Current Renewals</h2>
+		<ol>
+			<?php while ($row = $renewals->fetch(PDO::FETCH_ASSOC)) {
+				?>
+				<li>
+					<a href="<?php echo autoUrl("renewal/" . $row['ID']); ?>">
+						<?php echo $row['Name']; ?> (<?php echo date("j F Y",
+						strtotime($row['StartDate'])); ?> - <?php echo date("j F Y",
+						strtotime($row['EndDate'])); ?>)
+					</a>
+				</li><?
+			} ?>
+		</ol>
+		<h2>Add a new Renewal Period</h2>
+		<p>
+			<a href="<?php echo autoUrl("renewal/new"); ?>" class="btn
+			btn-success">
+				Add new Renewal Period
+			</a>
+		</p>
 	</div>
- </div>
+</div>
 
 <?php include BASE_PATH . "views/footer.php";
