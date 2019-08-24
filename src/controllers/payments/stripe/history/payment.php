e535fd4b82d3ed8f9b58a726cc442dad5a192efc
Chris Heppell <clheppell@icloud.com>
2019-08-24 22:31:52 +0100

    Update status page

--- src/controllers/payments/stripe/history/payment.php
+++ src/controllers/payments/stripe/history/payment.php
@@ -18,6 +18,70 @@ function getWalletName($name) {
   }
 }
 
+function outcomeTypeInfo($type) {
+  switch ($type) {
+    case 'authorized':
+      return '<i class="text-success fa fa-check-circle fa-fw" aria-hidden="true"></i> Payment authorised by issuer';
+      break;
+    case 'manual_review':
+      return 'Requires manual review';
+      break;
+    case 'issuer_declined':
+      return '<i class="text-danger fa fa-times-circle fa-fw" aria-hidden="true"></i> Payment declined by issuer';
+      break;
+    case 'blocked':
+      return '<i class="text-danger fa fa-times-circle fa-fw" aria-hidden="true"></i> Payment blocked by Stripe';
+      break;
+    case 'invalid':
+      return '<i class="text-danger fa fa-times-circle fa-fw" aria-hidden="true"></i> Request details were invalid';
+      break;
+    default:
+      return 'Unknown outcome type';
+      break;
+  }
+}
+
+function outcomeRiskLevel($riskLevel) {
+  switch ($riskLevel) {
+    case 'normal':
+      return '<i class="text-success fa fa-check-circle fa-fw" aria-hidden="true"></i> Normal';
+      break;
+    case 'elevated':
+      return '<i class="text-danger fa fa-info-circle fa-fw" aria-hidden="true"></i> Elevated risk';
+      break;
+    case '<i class="text-danger fa fa-info-circle fa-fw" aria-hidden="true"></i> highest':
+      return 'High risk';
+      break;
+    case 'not_assessed':
+      return '<i class="text-muted fa fa-question-circle fa-fw" aria-hidden="true"></i> Risk not assessed';
+      break;
+    default:
+      return '<i class="text-warning fa fa-info-circle fa-fw" aria-hidden="true"></i> Error in risk evaluation';
+      break;
+  }
+}
+
+function cardCheckInfo($value) {
+  switch ($value) {
+    case 'pass':
+      return '<i class="text-success fa fa-check-circle fa-fw" aria-hidden="true"></i>
+       Verified';
+      break;
+    case 'failed':
+      return '<i class="text-danger fa fa-times-circle fa-fw" aria-hidden="true"></i> Check failed';
+      break;
+    case 'unavailable':
+      return '<i class="text-muted fa fa-question-circle fa-fw" aria-hidden="true"></i> Check not possible';
+      break;
+    case 'unchecked':
+      return '<i class="text-muted fa fa-circle fa-fw" aria-hidden="true"></i> Unverified';
+      break;
+    default:
+      return 'Unknown status';
+      break;
+  }
+}
+
 global $db;
 
 $payment = $db->prepare("SELECT * FROM ((stripePayments INNER JOIN stripePaymentItems ON stripePaymentItems.Payment = stripePayments.ID) INNER JOIN users ON stripePayments.User = users.UserID) WHERE stripePayments.ID = ?");
@@ -39,8 +103,6 @@ $payment = \Stripe\PaymentIntent::retrieve([
   'expand' => ['customer', 'payment_method']
 ]);
 
-pre($payment);
-
 $pagetitle = 'Card Payment #' . htmlspecialchars($id);
 
 include BASE_PATH . 'views/header.php';
@@ -52,6 +114,8 @@ if (isset($payment->charges->data[0]->payment_method_details->card)) {
 
 $date = new DateTime($pm['DateTime'], new DateTimeZone('UTC'));
 $date->setTimezone(new DateTimeZone('Europe/London'));
+
+$countries = getISOAlpha2Countries();
 
 ?>
 
@@ -93,36 +157,91 @@ $date->setTimezone(new DateTimeZone('Europe/London'));
         <dd class="col-sm-7 col-md-8">&#0149;&#0149;&#0149;&#0149; <?=htmlspecialchars($card->wallet->dynamic_last4)?></dd>
         <?php } ?>
         <?php } ?>
+      </dl>
         
+      <?php if ($_SESSION['AccessLevel'] == 'Admin') { ?>
+      <h2>Transaction security information</h2>
+      <dl class="row">
         <?php if (isset($payment->charges->data[0]->outcome->risk_level) && $payment->charges->data[0]->outcome->risk_level) { ?>
         <dt class="col-sm-5 col-md-4">Risk level</dt>
-        <dd class="col-sm-7 col-md-8"><?=htmlspecialchars($payment->charges->data[0]->outcome->risk_level)?></dd>
+        <dd class="col-sm-7 col-md-8"><?=outcomeRiskLevel($payment->charges->data[0]->outcome->risk_level)?></dd>
         <?php } ?>
         
         <?php if (isset($payment->charges->data[0]->outcome->risk_score) && $payment->charges->data[0]->outcome->risk_score) { ?>
         <dt class="col-sm-5 col-md-4">Risk score</dt>
         <dd class="col-sm-7 col-md-8"><?=htmlspecialchars($payment->charges->data[0]->outcome->risk_score)?></dd>
         <?php } ?>
+
+        <?php if (isset($payment->charges->data[0]->outcome->type) && $payment->charges->data[0]->outcome->type) { ?>
+        <dt class="col-sm-5 col-md-4">Payment outcome</dt>
+        <dd class="col-sm-7 col-md-8"><?=outcomeTypeInfo($payment->charges->data[0]->outcome->type)?></dd>
+        <?php } ?>
+
+        <?php if (isset($payment->charges->data[0]->outcome->seller_message) && $payment->charges->data[0]->outcome->seller_message) { ?>
+        <dt class="col-sm-5 col-md-4">Status message*</dt>
+        <dd class="col-sm-7 col-md-8"><?=htmlspecialchars($payment->charges->data[0]->outcome->seller_message)?></dd>
+        <?php } ?>
         
         <?php if (isset($payment->charges->data[0]->receipt_url) && $payment->charges->data[0]->receipt_url) { ?>
         <dt class="col-sm-5 col-md-4">Stripe receipt</dt>
-        <dd class="col-sm-7 col-md-8"><a target="_blank" href="<?=htmlspecialchars($payment->charges->data[0]-> receipt_url)?>">Receipt</a></dd>
+        <dd class="col-sm-7 col-md-8"><a target="_blank" href="<?=htmlspecialchars($payment->charges->data[0]->receipt_url)?>">View receipt</a></dd>
+        <?php } ?>
+      </dl>
+
+      <p>
+        * You must not share status message information with the customer.
+      </p>
+
+      <h2>Billing address verification</h2>
+      <dl class="row">
+        <?php if (isset($payment->charges->data[0]->billing_details->address)) {
+          $billingAddress = $payment->charges->data[0]->billing_details->address; ?>
+        <dt class="col-sm-5 col-md-4">Billing Address</dt>
+        <dd class="col-sm-7 col-md-8">
+          <address class="mb-0">
+            <?php if (isset($payment->charges->data[0]->billing_details->name)) { ?>
+            <strong>
+              <?=htmlspecialchars($payment->charges->data[0]->billing_details->name)?>
+            </strong><br>
+            <?php } ?>
+            <?php if (isset($billingAddress->line1)) { ?>
+              <?=htmlspecialchars($billingAddress->line1)?><br>
+            <?php } ?>
+            <?php if (isset($billingAddress->line2)) { ?>
+              <?=htmlspecialchars($billingAddress->line2)?><br>
+            <?php } ?>
+            <?php if (isset($billingAddress->city)) { ?>
+              <?=htmlspecialchars($billingAddress->city)?><br>
+            <?php } ?>
+            <?php if (isset($billingAddress->postal_code)) { ?>
+              <?=htmlspecialchars($billingAddress->postal_code)?><br>
+            <?php } ?>
+            <?php if (isset($billingAddress->state)) { ?>
+              <?=htmlspecialchars($billingAddress->state)?><br>
+            <?php } ?>
+            <?php if (isset($billingAddress->country)) { ?>
+              <?=htmlspecialchars($countries[$billingAddress->country])?>
+            <?php } ?>
+          </address>
+        </dd>
         <?php } ?>
         
         <?php if (isset($payment->payment_method->card->checks->address_line1_check) && $payment->payment_method->card->checks->address_line1_check) { ?>
         <dt class="col-sm-5 col-md-4">Address line 1</dt>
-        <dd class="col-sm-7 col-md-8"><?=htmlspecialchars($payment->payment_method->card->checks->address_line1_check)?></dd>
+        <dd class="col-sm-7 col-md-8"><?=cardCheckInfo($payment->payment_method->card->checks->address_line1_check)?></dd>
         <?php } ?>
         
         <?php if (isset($payment->payment_method->card->checks->address_postal_code_check) && $payment->payment_method->card->checks->address_postal_code_check) { ?>
         <dt class="col-sm-5 col-md-4">Post code</dt>
-        <dd class="col-sm-7 col-md-8"><?=htmlspecialchars($payment->payment_method->card->checks->address_postal_code_check)?></dd>
+        <dd class="col-sm-7 col-md-8"><?=cardCheckInfo($payment->payment_method->card->checks->address_postal_code_check)?></dd>
         <?php } ?>
         
         <?php if (isset($payment->payment_method->card->checks->cvc_check) && $payment->payment_method->card->checks->cvc_check) { ?>
         <dt class="col-sm-5 col-md-4">CVC</dt>
-        <dd class="col-sm-7 col-md-8"><?=htmlspecialchars($payment->payment_method->card->checks->cvc_check)?></dd>
+        <dd class="col-sm-7 col-md-8"><?=cardCheckInfo($payment->payment_method->card->checks->cvc_check)?></dd>
         <?php } ?>
+      </dl>
+      <?php } ?>
         
         
       </dl>
