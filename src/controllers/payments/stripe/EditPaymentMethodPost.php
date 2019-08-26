636094edea3a3f74d56e332563c7817550a83123
Chris Heppell <chris.heppell@chesterlestreetasc.co.uk>
2019-08-26 14:54:46 +0000

    Payment receipt email updates

---
+++ src/controllers/payments/stripe/EditPaymentMethodPost.php
@@ -0,0 +1,28 @@
+<?php
+
+\Stripe\Stripe::setApiKey(env('STRIPE'));
+
+global $db;
+
+$getCard = $db->prepare("SELECT `Name`, Last4, Brand, ExpMonth, ExpYear, Funding, PostCode, Line1, Line2, CardName, MethodID FROM stripePayMethods INNER JOIN stripeCustomers ON stripeCustomers.CustomerID = stripePayMethods.Customer WHERE User = ? AND stripePayMethods.ID = ?");
+$getCard->execute([$_SESSION['UserID'], $id]);
+
+$card = $getCard->fetch(PDO::FETCH_ASSOC);
+
+if ($card == null) {
+  halt(404);
+}
+
+$newName = trim($_POST['name']);
+
+if ($newName != $card['Name'] && $newName != "" && $newName != null) {
+  try {
+    $update = $db->prepare("UPDATE stripePayMethods SET `Name` = ? WHERE ID = ?");
+    $update->execute([$newName, $id]);
+    $_SESSION['CardNameUpdate'] = true;
+  } catch (Exception $e) {
+    // Do nothing
+  }
+}
+
+header("Location: " . currentUrl());
