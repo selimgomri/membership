9b622937d3789abe824cc6030d5c1e2ace51287a
Chris Heppell <clheppell@icloud.com>
2019-09-03 00:57:20 +0100

    Code to set up db and add first user

---
+++ src/controllers/db/system-setup.php
@@ -0,0 +1,28 @@
+<?php
+
+global $db;
+
+// Verify that there are no db tables.
+$getTableCount = $db->prepare("SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND `TABLE_SCHEMA` = ?");
+$getTableCount->execute([
+  env('DB_NAME')
+]);
+
+if ($getTableCount->fetchColumn() == 0) {
+  // Run migrations system then add default admin
+  // Default admin uses env('CLUB_EMAIL') email if set
+  // Else no admin is added
+  include BASE_PATH . 'controllers/migrations/migrate.php';
+
+  if (env('CLUB_EMAIL')) {
+    try {
+      $addFirstUser = $db->prepare("INSERT INTO users (`Password`, AccessLevel, EmailAddress, EmailComms, Forename, Surname, Mobile, MobileComms) VALUES (?, 'Admin', ?, 1, 'Primary', 'Admin', '+44123456789', 1)");
+      $addFirstUser->execute([
+        password_hash('DEFAULTPASSWORD', PASSWORD_BCRYPT),
+        env('CLUB_EMAIL')
+      ]);
+    } catch (Exception $e) {
+      // Could not add user
+    }
+  }
+}
