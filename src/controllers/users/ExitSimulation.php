05416b168f0116cffa1ead7779a510d65b4a9494
Chris Heppell <chris.heppell@chesterlestreetasc.co.uk>
2019-10-01 17:22:57 +0000

    Bug fix

--- src/controllers/users/ExitSimulation.php
+++ src/controllers/users/ExitSimulation.php
@@ -14,14 +14,16 @@ try {
 $info = $query->fetch(PDO::FETCH_ASSOC);
 
 if ($info == null) {
-	reportError('Info is null');
 	halt(404);
 }
 
+$_SESSION['UserSimulation'] = null;
 $_SESSION['UserSimulation'] = [];
 unset($_SESSION['UserSimulation']);
 
-$_SESSION = null;
+$_SESSION = [];
+
+session_destroy();
 
 $_SESSION['Username'] = 		$info['Username'];
 $_SESSION['EmailAddress'] = $info['EmailAddress'];
