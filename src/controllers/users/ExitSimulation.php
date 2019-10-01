40f0c671077d5d81100e30ab180261130973cff0
Chris Heppell <clheppell@icloud.com>
2019-10-01 17:54:29 +0100

    User sim bug fix

--- src/controllers/users/ExitSimulation.php
+++ src/controllers/users/ExitSimulation.php
@@ -13,10 +13,11 @@ try {
 
 $info = $query->fetch(PDO::FETCH_ASSOC);
 
-if (!$info) {
+if ($info == null) {
 	halt(404);
 }
 
+$_SESSION['UserSimulation'] = null;
 unset($_SESSION['UserSimulation']);
 
 $_SESSION = null;
