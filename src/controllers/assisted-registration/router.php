7a247da9f2af90790d402407932aa0205cd37bba
Chris Heppell <chris.heppell@chesterlestreetasc.co.uk>
2019-11-02 10:45:31 +0000

    Code to redirect user to current stage

--- src/controllers/assisted-registration/router.php
+++ src/controllers/assisted-registration/router.php
@@ -13,6 +13,10 @@ if ($_SESSION['AccessLevel'] != 'Parent') {
     $this->post('/start', function() {
       include 'beginPost.php';
     });
+
+    $this->get('/*', function() {
+      header("Location: " . autoUrl("assisted-registration/start"));
+    });
   } else if (isset($_SESSION['AssRegUser']) && !isset($_SESSION['AssRegComplete'])) {
     $this->get('/select-swimmers', function() {
       include 'select.php';
@@ -21,9 +25,17 @@ if ($_SESSION['AccessLevel'] != 'Parent') {
     $this->post('/select-swimmers', function() {
       include 'selectPost.php';
     });
+
+    $this->get('/*', function() {
+      header("Location: " . autoUrl("assisted-registration/select-swimmers"));
+    });
   } else if (isset($_SESSION['AssRegUser']) && isset($_SESSION['AssRegComplete']) && $_SESSION['AssRegComplete']) {
     $this->get('/complete', function() {
       include 'complete.php';
+    });
+
+    $this->get('/*', function() {
+      header("Location: " . autoUrl("assisted-registration/complete"));
     });
   }
 }
