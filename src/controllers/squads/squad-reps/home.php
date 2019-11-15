91c7317473f907bdf0e594306812edf0c6198f89
Chris Heppell <clheppell@icloud.com>
2019-11-15 15:36:01 +0000

    Squad rep home updates
    Now links to composer for notify

--- src/controllers/squads/squad-reps/home.php
+++ src/controllers/squads/squad-reps/home.php
@@ -15,53 +15,79 @@ include BASE_PATH . 'views/header.php';
 
 ?>
 
-<div class="container">
-  <div class="row">
-    <div class="col-lg-8">
-      <h1>Welcome to Squad Rep Services</h1>
-      <p class="lead">This service allows you to view gala entries and their payment status for your squads.</p>
+<div class="front-page mb-n3">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8">
+        <h1>Welcome to Squad Rep Services</h1>
+        <p class="lead">This service allows you to view gala entries and their payment status for your squads.</p>
 
-      <h2>
-        Upcoming galas
-      </h2>
-      <?php if ($gala != null) { ?>
-        <ul>
-        <?php do { ?>
-          <li>
+        <h2>
+          Upcoming galas
+        </h2>
+        <?php if ($gala != null) { ?>
+          <div class="news-grid">
+          <?php do { ?>
             <a href="<?=autoUrl("galas/" . $gala['GalaID'] . "/squad-rep-view")?>">
-              <?=htmlspecialchars($gala['GalaName'])?> at <?=htmlspecialchars($gala['GalaVenue'])?>
+              <span class="mb-3">
+                <span class="title mb-0">
+                  <?=htmlspecialchars($gala['GalaName'])?>
+                </span>
+                <span>
+                  <?=htmlspecialchars($gala['GalaVenue'])?>
+                </span>
+              </span>
+              <span class="category">
+                Galas
+              </span>
             </a>
-          </li>
-        <?php } while ($gala = $getGalas->fetch(PDO::FETCH_ASSOC)); ?>
-        </ul>
-      <?php } else { ?>
-        <div class="alert alert-warning">
-          <p class="mb-0">
-            <strong>
-              There are no upcoming galas
-            </strong>
-          </p>
-          <p class="mb-0">
-            Please check back later
-          </p>
-        </div>
-      <?php } ?>
+          <?php } while ($gala = $getGalas->fetch(PDO::FETCH_ASSOC)); ?>
+          </div>
+        <?php } else { ?>
+          <div class="alert alert-warning">
+            <p class="mb-0">
+              <strong>
+                There are no upcoming galas
+              </strong>
+            </p>
+            <p class="mb-0">
+              Please check back later
+            </p>
+          </div>
+        <?php } ?>
 
-      <h2>
-        Other services
-      </h2>
-      <ul>
-        <li>
-          <a href="<?=autoUrl("notify")?>">
-            Email parents
+        <h2>
+          Other services
+        </h2>
+        <div class="news-grid">
+          <a href="<?=autoUrl("notify/newemail")?>">
+            <span class="mb-3">
+              <span class="title mb-0">
+                Email parents
+              </span>
+              <span>
+                Email parents of swimmers in your squads
+              </span>
+            </span>
+            <span class="category">
+              Notify
+            </span>
           </a>
-        </li>
-        <li>
           <a href="<?=autoUrl("squad-reps/list")?>">
-            View list of Squad Reps
+            <span class="mb-3">
+              <span class="title mb-0">
+                View all squads reps
+              </span>
+              <span>
+                View a list of all squad reps
+              </span>
+            </span>
+            <span class="category">
+              Squad Reps
+            </span>
           </a>
-        </li>
-      </ul>
+        </div>
+      </div>
     </div>
   </div>
 </div>
