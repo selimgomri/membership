8ff3105184425d40fb974f3791fb5ff6e1b4b173
Chris Heppell <chris.heppell@chesterlestreetasc.co.uk>
2019-11-29 23:54:21 +0000

    Create files

---
+++ src/controllers/galas/pricing-and-events/edit.php
@@ -0,0 +1,19 @@
+<?php
+
+$pagetitle = 'Pricing and Events';
+include BASE_PATH . 'views/header.php';
+
+?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-8">
+      <h1>Pricing and events</h1>
+      <p class="lead">Manage events and entry fees for this gala.</p>
+    </div>
+  </div>
+</div>
+
+<?php
+
+include BASE_PATH . 'views/footer.php';
