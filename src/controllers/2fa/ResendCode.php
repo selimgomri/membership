f136c7abe9b12567d8c715e2dfef57bd2aee9c7d
Chris Heppell <clheppell@icloud.com>
2020-01-01 20:35:44 +0000

    Updates to stop threads of 2FA emails, Invoice payment text truncates

--- src/controllers/2fa/ResendCode.php
+++ src/controllers/2fa/ResendCode.php
@@ -12,14 +12,16 @@ try {
   $query->execute([$_SESSION['2FAUserID']]);
   $row = $query->fetch(PDO::FETCH_ASSOC);
 
+  $date = new DateTime('now', new DateTimeZone('Europe/London'));
+
   $message = '
   <p>Hello. Confirm your login by entering the following code in your web browser.</p>
   <p><strong>' . $_SESSION['TWO_FACTOR_CODE'] . '</strong></p>
   <p>If you did not just try to log in, you can ignore this email. You may want to reset your password.</p>
   <p>This email was resent to this address at the request of the user.</p>
-  <p>Kind Regards,<br>The ' . env('CLUB_NAME') . ' Team</p>';
+  <p>Kind Regards, <br>The ' . env('CLUB_NAME') . ' Team</p>';
 
-  if (notifySend(null, "Verification Code", $message, $row['Forename'] . " " . $row['Surname'], $row['EmailAddress'])) {
+  if (notifySend(null, "Verification Code - Requested at " . $date->format("H:i:s \o\\n d/m/Y"), $message, $row['Forename'] . " " . $row['Surname'], $row['EmailAddress'])) {
     $_SESSION['TWO_FACTOR'] = true;
     $_SESSION['TWO_FACTOR_RESEND'] = true;
     header("Location: " . autoUrl("2fa"));
