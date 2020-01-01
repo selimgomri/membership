000e95c5c846008d0fc3de44a7d10e1f0354036c
Chris Heppell <clheppell@icloud.com>
2020-01-01 20:53:47 +0000

    General tidying up

--- src/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "The web demon must have read/write access to the install directory"
-
-echo "Pulling updates from GitHub repository"
-git checkout master
-git fetch --all
-git reset --hard origin/master
-echo "Update complete"
