93f14ce1176681aceccf7d6e6ec02200d4261332
Chris Heppell <clheppell@icloud.com>
2020-01-04 19:42:35 +0000

    Show localised country names

--- src/includes/regions/countries-iso3166.php
+++ src/includes/regions/countries-iso3166.php
@@ -1,5 +1,24 @@
 <?php
 
 function getISOAlpha2Countries() {
+	// Get the preferred locale
+	$prefLocales = array_reduce(
+    explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']), 
+      function ($res, $el) { 
+        list($l, $q) = array_merge(explode(';q=', $el), [1]); 
+        $res[$l] = (float) $q; 
+        return $res; 
+      }, []);
+	arsort($prefLocales);
+
+	// Check if we have a list for a locale (order of priority) and return that if we do
+	foreach($prefLocales as $locale => $priority) {
+		$path = BASE_PATH . 'vendor/umpirsky/country-list/data/' . str_replace('-', '_', $locale) . '/country.php';
+		if (file_exists($path)) {
+			return require $path;
+		}
+	}
+	
+	// Else return the default
 	return require BASE_PATH . 'vendor/umpirsky/country-list/data/en_GB/country.php';
 }
