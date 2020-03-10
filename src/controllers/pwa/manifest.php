80b0f6b389fc289ff220d83a9dbd2e5b1f1d4b3b
Chris Heppell <clheppell@icloud.com>
2020-03-10 16:47:31 +0000

    PWA fixes

--- src/controllers/pwa/manifest.php
+++ src/controllers/pwa/manifest.php
@@ -1,31 +1,42 @@
 <?php
 
-header("Content-Type: application/manifest+json");
+header('Content-Type: application/manifest+json');
 
-?>
-{
-  "name": <?=json_encode(env('CLUB_NAME') . " Membership")?>,
-  "short_name": "My Club",
-  "start_url": <?=json_encode(autoUrl("pwa"))?>,
-  "display": "standalone",
-  "background_color": "#fff",
-  "description": <?=json_encode("My " . env('CLUB_NAME') . " Membership")?>,
-  <?php
-  if (bool(env('IS_CLS'))) { 
-  ?>"icons": [{
-    "src": <?=json_encode(autoUrl("public/img/touchicons/touch-icon-72x72.png"))?>,
-    "sizes": "72x72",
-    "type": "image/png"
-  }, {
-    "src": <?=json_encode(autoUrl("public/img/touchicons/touch-icon-192x192.png"))?>,
-    "sizes": "192x192",
-    "type": "image/png"
-  }]<?php
-  } else { 
-  ?>"icons": [{
-    "src": <?=json_encode(autoUrl("public/img/corporate/scds.png"))?>,
-    "sizes": "800x800",
-    "type": "image/png"
-  }]<?php
-  } ?>
+$icons = [];
+
+$themeColour = "#bd0000";
+if (false) {
+  $themeColour = "";
 }
+
+if (bool(env('IS_CLS'))) { 
+  $icons[] = [
+    'src' => autoUrl('public/img/touchicons/touch-icon-72x72.png'),
+    'sizes' => '72x72',
+    'type' => 'image/png'
+  ];
+  $icons[] = [
+    'src' => autoUrl('public/img/touchicons/touch-icon-192x192.png'),
+    'sizes' => '192x192',
+    'type' => 'image/png'
+  ];
+} else {
+  $icons[] = [
+    'src' => autoUrl('public/img/corporate/scds.png'),
+    'sizes' => '800x800',
+    'type' => 'image/png'
+  ];
+}
+
+$data = [
+  'name' => env('CLUB_NAME') . ' Membership',
+  'short_name' => 'My Club',
+  'start_url' => autoUrl('pwa'),
+  'display' => 'standalone',
+  'background_color' => '#fff',
+  'description' => 'My ' . env('CLUB_NAME') . ' Membership',
+  'icons' => $icons,
+  'theme_color' => $themeColour
+];
+
+echo json_encode($data);
