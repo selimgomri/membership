5652263ef6e2ab20dc5d212af77b5d82fa98ae70
Chris Heppell <clheppell@icloud.com>
2020-03-17 10:53:57 +0000

    Bug fix

--- src/public/js/emergency/emergency-message.js
+++ src/public/js/emergency/emergency-message.js
@@ -1,4 +1,4 @@
-var ajax = new ajaxRequest();
+var ajax = new XMLHttpRequest();
 ajax.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     data = JSON.parse(this.responseText);
