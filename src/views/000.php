951005fc6af8a0ebed17b6a556be6e6377eeb7cd
Chris Heppell <clheppell@icloud.com>
2020-03-21 00:45:18 +0000

    Slight redesign

--- src/views/000.php
+++ src/views/000.php
@@ -15,12 +15,17 @@ if ($currentUser == null) {
 		<div class="col-lg-8">
 			<h1>The SCDS Membership System is closed for maintenance</h1>
 			<p class="lead">We'll be back shortly.</p>
-			<hr>
-			<p>HTTP Status 200 - OK.</p>
-			<hr>
-			<p class="mt-2">Contact our <a href="mailto:support@chesterlestreetasc.co.uk" title="Support Hotline">support address</a> if you need urgent access to database records. Gala entries cannot be processed at this time - We plan scheduled downtime to avoid gala entry deadlines.</p>
+			<p>
+				Contact our <a href="mailto:support@chesterlestreetasc.co.uk" title="Support Hotline">support address</a> if you need urgent access to database records.
+			</p>
+			<p>
+				Gala entries and payments cannot be processed at this time - We plan scheduled downtime to avoid gala entry deadlines.
+			</p>
 
 			<p><a href="https://corporate.myswimmingclub.co.uk">myswimmingclub.co.uk</a></p>
+
+			<hr>
+			<p>HTTP Status 200 - OK.</p>
 
 		</div>
 	</div>
