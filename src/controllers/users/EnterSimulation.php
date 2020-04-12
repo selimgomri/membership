83351de07442eee835f2755fcf1d9cd9e19bb0dc
Chris Heppell <chris.heppell@chesterlestreetasc.co.uk>
2020-04-12 17:08:17 +0100

    Fix a bug in EnterSimulation.php

--- src/controllers/users/EnterSimulation.php
+++ src/controllers/users/EnterSimulation.php
@@ -3,7 +3,7 @@
 global $db;
 
 try {
-	$query = $db->prepare("SELECT * FROM `users` WHERE `UserID` = ?");
+	$query = $db->prepare("SELECT * FROM `users` WHERE `UserID` = ? AND Active");
 	$query->execute([$id]);
 } catch (Exception $e) {
 	halt(500);
@@ -16,17 +16,17 @@ if (!$info) {
 }
 
 $_SESSION['UserSimulation'] = [
-	'RealUser' 		=> $_SESSION['UserID'],
-	'SimUser'			=> $info['UserID'],
-	'SimUserName'	=> $info['Forename'] . ' ' . $info['Surname']
+	'RealUser'    => $_SESSION['UserID'],
+	'SimUser'     => $info['UserID'],
+	'SimUserName' => $info['Forename'] . ' ' . $info['Surname']
 ];
 
-$_SESSION['Username'] = 		$info['Username'];
+$_SESSION['Username'] =     $info['Username'];
 $_SESSION['EmailAddress'] = $info['EmailAddress'];
-$_SESSION['Forename'] = 		$info['Forename'];
-$_SESSION['Surname'] = 			$info['Surname'];
-$_SESSION['UserID'] = 			$info['UserID'];
-$_SESSION['LoggedIn'] = 		1;
+$_SESSION['Forename'] =     $info['Forename'];
+$_SESSION['Surname'] =      $info['Surname'];
+$_SESSION['UserID'] =       $info['UserID'];
+$_SESSION['LoggedIn'] =     1;
 
 $userObject = new \User($id, $db);
 
