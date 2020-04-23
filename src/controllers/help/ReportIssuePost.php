b3a8e235f025065cc153f15522ce3c0a14f842ac
Chris Heppell <clheppell@icloud.com>
2020-04-23 17:11:29 +0100

    Report an issue mods

--- src/controllers/help/ReportIssuePost.php
+++ src/controllers/help/ReportIssuePost.php
@@ -1,5 +1,6 @@
 <?php
 
+$currentUser = app()->user;
 use Respect\Validation\Validator as v;
 
 $target = $_POST['report_url'];
@@ -10,11 +11,14 @@ $_SESSION['ErrorReportStatus'] = false;
 if (v::url()->validate($target)) {
 
 	$message = "<p>An error has been reported on the following page: " . $target . ".</p>";
+	if ($currentUser) {
+		$message .= "<p>The user was " . htmlspecialchars($currentUser->getFirstName() . ' ' . $currentUser->getLastName()) . ", " . htmlspecialchars($currentUser->getEmail()) . ".</p>"
+	}
 	$message .= "<p>The user said: " . $usr_message . "</p>";
 	$message .= "<p>Reported on " . date("l j F Y") . ".</p>";
-	$message .= "<p>Sent Automatically by CLS ASC.</p>";
+	$message .= "<p>Sent Automatically by Swimming Club Data Systems.</p>";
 
-	notifySend("", "Website Error Report", $message, "Website Admin Team", "web@chesterlestreetasc.co.uk", ["Email" => "issues@web.service.chesterlestreetasc.co.uk", "Name" => "Chester-le-Street ASC"]);
+	notifySend("", "Website Error Report", $message, "Website Admin Team", "web@chesterlestreetasc.co.uk", ["Email" => "report-an-issue@" . env('EMAIL_DOMAIN'), "Name" => "Error Reports at " . env('CLUB_NAME')]);
 
 	$_SESSION['ErrorReportStatus'] = true;
 	$_SESSION['ErrorReportTarget'] = $target;
