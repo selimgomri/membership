89d8b54e860c614ae7f4909df21cf4302b4dd6d7
Chris Heppell <clheppell@icloud.com>
2020-08-15 00:08:41 +0100

    User mandates list ready for Stripe DD

--- src/controllers/payments/admin/user-mandates/UserMandateList.php
+++ src/controllers/payments/admin/user-mandates/UserMandateList.php
@@ -20,10 +20,24 @@ include BASE_PATH . 'views/header.php';
 ?>
 
 <div class="container">
+
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="<?= htmlspecialchars(autoUrl('payments')) ?>">Payments</a></li>
+      <li class="breadcrumb-item active" aria-current="page">Mandates</li>
+    </ol>
+  </nav>
+
   <div class="row">
     <div class="col-lg-8">
       <h1>User mandates</h1>
-      <p class="lead">Direct Debit mandates by user</p>
+      <p class="lead">(GoCardless <em>Legacy</em>) Direct Debit mandates by user</p>
+
+      <?php if (app()->tenant->getStripeAccount() || app()->tenant->getBooleanKey('ALLOW_STRIPE_DIRECT_DEBIT_SET_UP')) { ?>
+        <p>
+          <a href="<?= htmlspecialchars(autoUrl('payments/user-mandates')) ?>">View Stripe Mandates</a>
+        </p>
+      <?php } ?>
     </div>
   </div>
 
@@ -33,25 +47,25 @@ include BASE_PATH . 'views/header.php';
     </div>
   <?php } else { ?>
     <div class="list-group">
-    <?php do { ?>
-      <a href="<?=htmlspecialchars(autoUrl("users/" . $mandate['UserID']))?>" class="list-group-item list-group-item-action <?php if (!$mandate['BankName']) { ?> list-group-item-danger <?php }?>">
-        <div class="row align-items-center">
-          <div class="col-md-6">
-            <h2 class="mb-0 h4"><?=htmlspecialchars($mandate['Surname'] . ', ' . $mandate['Forename'])?></h2>
-            <div class="mb-3 d-md-none"></div>
+      <?php do { ?>
+        <a href="<?= htmlspecialchars(autoUrl("users/" . $mandate['UserID'])) ?>" class="list-group-item list-group-item-action <?php if (!$mandate['BankName']) { ?> list-group-item-danger <?php } ?>">
+          <div class="row align-items-center">
+            <div class="col-md-6">
+              <h2 class="mb-0 h4"><?= htmlspecialchars($mandate['Surname'] . ', ' . $mandate['Forename']) ?></h2>
+              <div class="mb-3 d-md-none"></div>
+            </div>
+            <div class="col text-md-right">
+              <?php if ($mandate['BankName']) { ?>
+                <p class="mono mb-0"><?= htmlspecialchars(getBankName($mandate['BankName'])) ?></p>
+                <p class="mono mb-0"><?= htmlspecialchars($mandate['AccountHolderName']) ?>, &#0149;&#0149;&#0149;&#0149;&#0149;&#0149;<?= htmlspecialchars($mandate['AccountNumEnd']) ?></p>
+              <?php } else { ?>
+                <p class="mono mb-0">No mandate</p>
+                <p class="mono mb-0">Contact user</p>
+              <?php } ?>
+            </div>
           </div>
-          <div class="col text-md-right">
-            <?php if ($mandate['BankName']) { ?>
-            <p class="mono mb-0"><?=htmlspecialchars(getBankName($mandate['BankName']))?></p>
-            <p class="mono mb-0"><?=htmlspecialchars($mandate['AccountHolderName'])?>, &#0149;&#0149;&#0149;&#0149;&#0149;&#0149;<?=htmlspecialchars($mandate['AccountNumEnd'])?></p>
-            <?php } else { ?>
-            <p class="mono mb-0">No mandate</p>
-            <p class="mono mb-0">Contact user</p>
-            <?php } ?>
-          </div>
-        </div>
-      </a>
-    <?php } while ($mandate = $getMandates->fetch(PDO::FETCH_ASSOC)); ?>
+        </a>
+      <?php } while ($mandate = $getMandates->fetch(PDO::FETCH_ASSOC)); ?>
     </div>
   <?php } ?>
 </div>
