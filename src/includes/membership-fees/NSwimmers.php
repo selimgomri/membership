2b55ba48960c8673cfcf9a8f1c4a96687e550bad
Chris Heppell <clheppell@icloud.com>
2020-08-16 19:54:24 +0100

    Test fix

--- src/includes/membership-fees/NSwimmers.php
+++ src/includes/membership-fees/NSwimmers.php
@@ -8,7 +8,10 @@
 
 namespace SCDS\Membership;
 
-class NSwimmers extends ClubMembership {
+use Exception;
+
+class NSwimmers extends ClubMembership
+{
 
   private $fees;
   private $items;
@@ -17,79 +20,81 @@ class NSwimmers extends ClubMembership {
   /**
    * Object constructor
    */
-  public function __construct($db, $user, $upgrade) {
-    $getValue = $db->prepare("SELECT `Value` FROM tenantOptions WHERE `Option` = ?");
-
+  public function __construct($db, $user, $upgrade)
+  {
     // Verify type is IndividualAndFamily
-    $getValue->execute(['ClubFeesType']);
-    if ($getValue->fetchColumn() != 'NSwimmers') {
-      throw new Exception('WrongType');
+    $type = app()->tenant->getKey('ClubFeesType');
+    if ($type != 'NSwimmers') {
+      throw new \Exception('WrongType');
     }
 
     // Get the fees
-    $getValue->execute(['ClubFeeNSwimmers']);
-    $this->fees = json_decode($getValue->fetchColumn());
+    try {
+      $this->fees = json_decode(app()->tenant->getKey('ClubFeeNSwimmers'));
 
-    $this->fetchUpgradeType($db);
-    $this->upgrade = $upgrade;
+      $this->fetchUpgradeType($db);
+      $this->upgrade = $upgrade;
 
-    $counts = $this->getSwimmers($db, $user);
-    $countReg = $counts['RRCount'];
-    $count = $counts['RCount'];
-    $totalMembers = $counts['Total'];
+      $counts = $this->getSwimmers($db, $user);
+      $countReg = $counts['RRCount'];
+      $count = $counts['RCount'];
+      $totalMembers = $counts['Total'];
 
-    $numMemStr = null;
-    if ($totalMembers >= sizeof($this->fees)) {
-      $numMemStr = sizeof($this->fees) . ' or more members';
-    } else if ($totalMembers > 1) {
-      $numMemStr = $totalMembers . ' members';
-    } else {
-      $numMemStr = '1 member';
-    }
+      $numMemStr = null;
+      if ($totalMembers >= sizeof($this->fees)) {
+        $numMemStr = sizeof($this->fees) . ' or more members';
+      } else if ($totalMembers > 1) {
+        $numMemStr = $totalMembers . ' members';
+      } else {
+        $numMemStr = '1 member';
+      }
 
-    // Work out full membership fee
-    $fullAmount = 0;
-    if ($totalMembers > 0 && $totalMembers > sizeof($this->fees)) {
-      $fullAmount = $this->fees[sizeof($this->fees)-1];
-    } else if ($totalMembers > 0) {
-      $fullAmount = $this->fees[$totalMembers-1];
-    }
+      // Work out full membership fee
+      $fullAmount = 0;
+      if ($totalMembers > 0 && $totalMembers > sizeof($this->fees)) {
+        $fullAmount = $this->fees[sizeof($this->fees) - 1];
+      } else if ($totalMembers > 0) {
+        $fullAmount = $this->fees[$totalMembers - 1];
+      }
 
-    // Work out previously paid membership fee
-    $paidAmount = 0;
-    if ($count > 0 && $count > sizeof($this->fees)) {
-      $paidAmount = $this->fees[sizeof($this->fees)-1];
-    } else if ($count > 0) {
-      $paidAmount = $this->fees[$count-1];
-    }
+      // Work out previously paid membership fee
+      $paidAmount = 0;
+      if ($count > 0 && $count > sizeof($this->fees) && isset($this->fees[sizeof($this->fees) - 1])) {
+        $paidAmount = $this->fees[sizeof($this->fees) - 1];
+      } else if ($count > 0) {
+        $paidAmount = $this->fees[$count - 1];
+      }
 
-    if ($this->upgradeType == 'FullFee' || !$this->upgrade) {
-      $this->total = (int) $fullAmount;
-      $this->items[] = [
-        'description' => 'Club membership (' . $numMemStr . ')',
-        'amount' => $this->total
-      ];
-    } else if ($this->upgradeType == 'TopUp') {
-      $this->total = (int) max([0, $fullAmount - $paidAmount]);
-      $this->items[] = [
-        'description' => 'Club membership (' . $numMemStr . ' (top up fee))',
-        'amount' => $this->total
-      ];
-    } else if ($this->upgradeType == 'None') {
-      $this->total = 0;
-      $this->items[] = [
-        'description' => 'Club membership (' . $numMemStr . ' (no fee to pay))',
-        'amount' => $this->total
-      ];
+      if ($this->upgradeType == 'FullFee' || !$this->upgrade) {
+        $this->total = (int) $fullAmount;
+        $this->items[] = [
+          'description' => 'Club membership (' . $numMemStr . ')',
+          'amount' => $this->total
+        ];
+      } else if ($this->upgradeType == 'TopUp') {
+        $this->total = (int) max([0, $fullAmount - $paidAmount]);
+        $this->items[] = [
+          'description' => 'Club membership (' . $numMemStr . ' (top up fee))',
+          'amount' => $this->total
+        ];
+      } else if ($this->upgradeType == 'None') {
+        $this->total = 0;
+        $this->items[] = [
+          'description' => 'Club membership (' . $numMemStr . ' (no fee to pay))',
+          'amount' => $this->total
+        ];
+      }
+    } catch (Exception $e) {
     }
   }
 
-  public function getFeeItems() {
+  public function getFeeItems()
+  {
     return $this->items;
   }
 
-  public function getFee() {
+  public function getFee()
+  {
     return $this->total;
   }
-
 }
