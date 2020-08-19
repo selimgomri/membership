a1219da9036e971377722455b653c587f6c625fd
Chris Heppell <clheppell@icloud.com>
2020-08-19 19:28:41 +0100

    Jump down to day

--- src/controllers/attendance/historyViews/jump-to-week.php
+++ src/controllers/attendance/historyViews/jump-to-week.php
@@ -4,7 +4,7 @@ try {
   $date = new DateTime($_POST['go-to-week-date'], new DateTimeZone('Europe/London'));
 
   http_response_code(302);
-  header("location: " . autoUrl('attendance/history/squads/' . $id . '?year=' . $date->format('o') . '&week=' . $date->format('W')));
+  header("location: " . autoUrl('attendance/history/squads/' . $id . '?year=' . $date->format('o') . '&week=' . $date->format('W') . '#day-' . $date->format('w')));
 } catch (Exception $e) {
   http_response_code(302);
   header("location: " . autoUrl('attendance/history/squads/' . $id));
