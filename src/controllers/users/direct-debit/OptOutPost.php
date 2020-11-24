fa12178c561606f030c7f380306e7b0f1649989d
Chris Heppell <clheppell@icloud.com>
2020-11-24 19:25:07 +0000

    DD opt out change

--- src/controllers/users/direct-debit/OptOutPost.php
+++ src/controllers/users/direct-debit/OptOutPost.php
@@ -23,27 +23,11 @@ try {
       
       include 'GetRenewal.php';
 
-      $progress = $db->prepare("UPDATE `renewalProgress` SET `Stage` = `Stage` + 1 WHERE `RenewalID` = ? AND `UserID` = ?");
+      $progress = $db->prepare("UPDATE `renewalProgress` SET `Stage` = `Stage` + 1, `Substage` = 0 WHERE `RenewalID` = ? AND `UserID` = ?");
       $progress->execute([
         $renewal,
         $person
       ]);
-
-      if ($reg) {
-        $sql = "UPDATE `users` SET `RR` = 0 WHERE `UserID` = ?";
-        $query = $db->prepare($sql);
-        $query->execute([$person]);
-
-        $query = $db->prepare("UPDATE `members` SET `RR` = 0, `RRTransfer` = 0 WHERE `UserID` = ?");
-        $query->execute([$person]);
-
-        // Remove from status tracker
-        $delete = $db->prepare("DELETE FROM renewalProgress WHERE UserID = ? AND RenewalID = ?");
-        $delete->execute([
-          $person,
-          $renewal
-        ]);
-      }
       $_SESSION['TENANT-' . app()->tenant->getId()]['Successful'] = true;
     } catch (Exception $e) {
       // Catches halt
