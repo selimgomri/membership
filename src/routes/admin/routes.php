71260de8cdfcabb92ee0097e53e753625aa1e055
Chris Heppell <clheppell@icloud.com>
2020-12-12 13:44:04 +0000

    Admin view for audit logs

--- src/routes/admin/routes.php
+++ src/routes/admin/routes.php
@@ -43,6 +43,10 @@ $this->group('/notify', function () {
   include 'notify/routes.php';
 });
 
+$this->group('/audit', function () {
+  include 'audit/routes.php';
+});
+
 $this->group('/users', function () {
   include 'users/routes.php';
 });
