d15ec3f4f3f8919681fc1d431d5d9c85bbe1d6ee
Chris Heppell <clheppell@icloud.com>
2021-04-29 18:07:59 +0100

    Fix up sudo loader

--- src/controllers/FileLoader.php
+++ src/controllers/FileLoader.php
@@ -8,7 +8,6 @@ use Aws\Exception\AwsException;
 $client = new S3Client([
   'version'     => 'latest',
   'region'      => getenv('AWS_S3_REGION'),
-  'visibility' => 'public',
 ]);
 
 $file = '';
@@ -21,46 +20,24 @@ $file = $file . $filename;
 
 $key = $tenant->getId() . '/' . $filename;
 
-if (true) {
+if (substr($filename, 0, 18) !== "notify/attachments") {
+  halt(404);
+}
 
-  if (substr($filename, 0, 18) !== "notify/attachments") {
-    halt(404);
-  }
+$cmd = $client->getCommand('GetObject', [
+  'Bucket' => getenv('AWS_S3_BUCKET'),
+  'Key' => $key
+]);
 
-  $cmd = $client->getCommand('GetObject', [
-    'Bucket' => getenv('AWS_S3_BUCKET'),
-    'Key' => $key
-  ]);
+$exists = $client->doesObjectExist(getenv('AWS_S3_BUCKET'), $key);
 
-  $exists = $client->doesObjectExist(getenv('AWS_S3_BUCKET'), $key);
+if ($exists) {
+  $request = $client->createPresignedRequest($cmd, '+5 minutes');
 
-  if ($exists) {
-    $request = $client->createPresignedRequest($cmd, '+5 minutes');
+  $presignedUrl = (string)$request->getUri();
 
-    $presignedUrl = (string)$request->getUri();
-
-    http_response_code(302);
-    header("location: " . $presignedUrl);
-  } else {
-    halt(404);
-  }
+  http_response_code(302);
+  header("location: " . $presignedUrl);
 } else {
-
-  if (file_exists($file)) {
-    header('Content-Description: File Transfer');
-    header('Content-Type: ' . mime_content_type($file));
-    if (mime_content_type($file) == 'application/pdf' || mb_substr(mime_content_type($file), 0, mb_strlen('image')) === 'image') {
-      header('Content-Disposition: inline');
-    } else {
-      header('Content-Disposition: attachment; filename="' . basename($filename) . '"');
-    }
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    header('Content-Length: ' . filesize($file));
-    readfile($file);
-    exit;
-  } else {
-    halt(404);
-  }
+  halt(404);
 }
--- src/controllers/FileLoaderSudo.php
+++ src/controllers/FileLoaderSudo.php
@@ -2,6 +2,14 @@
 
 $tenant = Tenant::fromId($tenant);
 
+use Aws\S3\S3Client;
+use Aws\Exception\AwsException;
+
+$client = new S3Client([
+  'version'     => 'latest',
+  'region'      => getenv('AWS_S3_REGION'),
+]);
+
 $file = '';
 try {
   $file = $tenant->getFilePath();
@@ -10,20 +18,26 @@ try {
 }
 $file = $file . $filename;
 
-if (file_exists($file)) {
-  header('Content-Description: File Transfer');
-  header('Content-Type: ' . mime_content_type($file));
-  if (mime_content_type($file) == 'application/pdf' || mb_substr(mime_content_type($file), 0, mb_strlen('image')) === 'image') {
-    header('Content-Disposition: inline');
-  } else {
-    header('Content-Disposition: attachment; filename="'.basename($filename).'"');
-  }
-  header('Expires: 0');
-  header('Cache-Control: must-revalidate');
-  header('Pragma: public');
-  header('Content-Length: ' . filesize($file));
-  readfile($file);
-  exit;
+$key = $tenant->getId() . '/' . $filename;
+
+if (substr($filename, 0, 18) !== "notify/attachments") {
+  halt(404);
+}
+
+$cmd = $client->getCommand('GetObject', [
+  'Bucket' => getenv('AWS_S3_BUCKET'),
+  'Key' => $key
+]);
+
+$exists = $client->doesObjectExist(getenv('AWS_S3_BUCKET'), $key);
+
+if ($exists) {
+  $request = $client->createPresignedRequest($cmd, '+5 minutes');
+
+  $presignedUrl = (string)$request->getUri();
+
+  http_response_code(302);
+  header("location: " . $presignedUrl);
 } else {
   halt(404);
 }
