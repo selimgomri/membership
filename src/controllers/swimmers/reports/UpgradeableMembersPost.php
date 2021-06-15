d935af8fb48338e316e03f649b093575c027074e
Chris Heppell <clheppell@icloud.com>
2021-06-15 13:25:37 +0100

    Remove and modify admin tools for NGB change

--- src/controllers/swimmers/reports/UpgradeableMembersPost.php
+++ src/controllers/swimmers/reports/UpgradeableMembersPost.php
@@ -3,34 +3,46 @@
 $db = app()->db;
 $tenant = app()->tenant;
 
-$allowedMembership = [
-  0,
-  1,
-  2,
-  3
-];
+$checkCategory = $db->prepare("SELECT COUNT(*) FROM `clubMembershipClasses` WHERE `Type` = ? AND `ID` = ? AND `Tenant` = ?");
 
 try {
 
   $db->beginTransaction();
 
-  $changeCat = $db->prepare("UPDATE members SET ASACategory = ? WHERE MemberID = ?");
+  $changeCat = $db->prepare("UPDATE members SET NGBCategory = ? WHERE MemberID = ?");
 
   $date = new DateTime('-9 years last day of December', new DateTimeZone('Europe/London'));
   $now = new DateTime('now', new DateTimeZone('Europe/London'));
 
-  $getMembers = $db->prepare("SELECT MemberID id, MForename fn, MSurname sn, DateOfBirth dob, ASACategory cat FROM members WHERE members.Active AND members.Tenant = ? AND DateOfBirth <= ? AND ASACategory = ? ORDER BY MForename ASC, MSurname ASC");
+  $findNGBCategory = $db->prepare("SELECT `ID` FROM `clubMembershipClasses` WHERE `Type` = ? AND `Name` LIKE ? AND `Tenant` = ?");
+  $findNGBCategory->execute([
+    'national_governing_body',
+    '%1%',
+    $tenant->getId(),
+  ]);
+  $category = $findNGBCategory->fetchColumn();
+
+  $getMembers = $db->prepare("SELECT MemberID id, MForename fn, MSurname sn, DateOfBirth dob, NGBCategory cat FROM members WHERE members.Active AND members.Tenant = ? AND DateOfBirth <= ? AND NGBCategory = ? ORDER BY MForename ASC, MSurname ASC");
   $getMembers->execute([
     $tenant->getId(),
     $date->format("Y-m-d"),
-    1
+    $category
   ]);
 
   $changed = false;
 
   while ($member = $getMembers->fetch(PDO::FETCH_ASSOC)) {
     if (isset($_POST["se-cat-" . $member['id']])) {
-      if ($_POST["se-cat-" . $member['id']] != $member['cat'] && in_array($_POST["se-cat-" . $member['id']], $allowedMembership)) {
+
+      $checkCategory->execute([
+        'national_governing_body',
+        $_POST["se-cat-" . $member['id']],
+        $tenant->getId(),
+      ]);
+
+      $exists = $checkCategory->fetchColumn();
+
+      if ($_POST["se-cat-" . $member['id']] != $member['cat'] && $exists) {
         // Update the membership category
         $changeCat->execute([
           $_POST["se-cat-" . $member['id']],
