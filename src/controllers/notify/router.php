43f5ae6156f9ab646ff299fb84a9f544f36ce491
Chris Heppell <clheppell@icloud.com>
2022-01-02 23:58:49 +0000

    Send group emails

--- src/controllers/notify/router.php
+++ src/controllers/notify/router.php
@@ -2,6 +2,28 @@
 
 $db = app()->db;
 
+$this->get('/lambda', function () {
+
+	// $client = new GuzzleHttp\Client();
+	// $response = $client->post('/2015-03-31/functions/arn:aws:lambda:eu-west-2:684636513987:function:Email-Queue/invocations', [
+	// 	'headers' => [
+	// 		'X-Amz-Invocation-Type' => 'Event',
+	// 	]
+	// ]);
+
+	$sharedConfig = [
+		'region' => 'eu-west-2',
+		'version' => 'latest'
+	];
+
+	// Create an SDK class used to share configuration across clients.
+	$sdk = new Aws\Sdk($sharedConfig);
+
+	$client = $sdk->createLambda();
+
+	// pre($client);
+});
+
 $rep = false;
 $repBlocked = false;
 if ($_SESSION['TENANT-' . app()->tenant->getId()]['AccessLevel'] == 'Parent') {
@@ -32,27 +54,27 @@ define('REP_BLOCKED', $repBlocked);
 
 $access = $_SESSION['TENANT-' . app()->tenant->getId()]['AccessLevel'];
 
-$this->post('/save-user-settings', function() {
+$this->post('/save-user-settings', function () {
 	include 'save-user-settings.php';
 });
 
 if ($access != "Admin" && $access != "Coach" && $access != "Galas" && !$rep) {
-	$this->get('/', function() {
-		
+	$this->get('/', function () {
+
 		include 'Help.php';
 	});
 }
 
 if ($access == "Admin" || $access == "Coach" || $access == "Galas" || $rep) {
-	$this->get('/', function() {
+	$this->get('/', function () {
 		include 'Home.php';
 	});
 
-	$this->post('/file-uploads', function() {
+	$this->post('/file-uploads', function () {
 		include 'FileUploads.php';
 	});
 
-	$this->post('/send-email', function() {
+	$this->post('/send-email', function () {
 		$access = $_SESSION['TENANT-' . app()->tenant->getId()]['AccessLevel'];
 		if (!($access == "Admin" || $access == "Coach" || $access == "Galas") && REP_BLOCKED) {
 			halt(404);
@@ -61,11 +83,11 @@ if ($access == "Admin" || $access == "Coach" || $access == "Galas" || $rep) {
 		}
 	});
 
-  $this->group(['/new', '/newemail'], function() {
+	$this->group(['/new', '/newemail'], function () {
 
 		$access = $_SESSION['TENANT-' . app()->tenant->getId()]['AccessLevel'];
 		if (!($access == "Admin" || $access == "Coach" || $access == "Galas") && REP_BLOCKED) {
-			$this->any(['/', '/*'], function() {
+			$this->any(['/', '/*'], function () {
 				include 'RepBlocked.php';
 			});
 		}
@@ -76,13 +98,13 @@ if ($access == "Admin" || $access == "Coach" || $access == "Galas" || $rep) {
   	});
 		*/
 
-		$this->get('/react-data', function() {
-  		include 'EmailAPIData.php';
-  	});
+		$this->get('/react-data', function () {
+			include 'EmailAPIData.php';
+		});
 
-		$this->post('/image-upload', function() {
-  		include 'ImageUpload.php';
-  	});
+		$this->post('/image-upload', function () {
+			include 'ImageUpload.php';
+		});
 
 		/*
   	$this->post('/', function() {
@@ -90,107 +112,105 @@ if ($access == "Admin" || $access == "Coach" || $access == "Galas" || $rep) {
   	});
 		*/
 
-    $this->get('/individual/{user}?:int/', function($user = null) {
-  		
-  		include 'EmailIndividual.php';
-  	});
+		$this->get('/individual/{user}?:int/', function ($user = null) {
 
-  	$this->post('/individual/{user}?:int/', function($user = null) {
-  		
-  		include 'EmailQueuerIndividual.php';
-  	});
+			include 'EmailIndividual.php';
+		});
 
+		$this->post('/individual/{user}?:int/', function ($user = null) {
+
+			include 'EmailQueuerIndividual.php';
+		});
 	});
-	
-	$this->get('/reply-to', function() {
+
+	$this->get('/reply-to', function () {
 		include 'ReplyTo.php';
 	});
 
-	$this->post('/reply-to', function() {
+	$this->post('/reply-to', function () {
 		include 'ReplyToPost.php';
 	});
 
-  if ($_SESSION['TENANT-' . app()->tenant->getId()]['AccessLevel'] == "Admin") {
-  	$this->get('/pending', function() {
-  		
-  		include 'EmailList.php';
-  	});
+	if ($_SESSION['TENANT-' . app()->tenant->getId()]['AccessLevel'] == "Admin") {
+		$this->get('/pending', function () {
 
-  	$this->get('/email/{id}:int', function($id) {
-  		
-  		include 'EmailID.php';
-  	});
-  }
+			include 'EmailList.php';
+		});
 
-  $this->group('/history', function() {
+		$this->get('/email/{id}:int', function ($id) {
+
+			include 'EmailID.php';
+		});
+	}
+
+	$this->group('/history', function () {
 		$access = $_SESSION['TENANT-' . app()->tenant->getId()]['AccessLevel'];
 		if (!($access == "Admin" || $access == "Coach" || $access == "Galas") && REP_BLOCKED) {
-			$this->any(['/', '/*'], function() {
+			$this->any(['/', '/*'], function () {
 				include 'RepBlocked.php';
 			});
 		}
 
-    $this->get('/', function() {
+		$this->get('/', function () {
 			include 'MessageHistory.php';
 		});
-  });
+	});
 
 	if (!$rep) {
 
-		$this->group('/lists', function() {
-			
+		$this->group('/lists', function () {
 
-			$this->get('/', function() {
-				
+
+			$this->get('/', function () {
+
 				include 'ListOfLists.php';
 			});
 
-			$this->get('/new', function() {
-				
+			$this->get('/new', function () {
+
 				include 'NewList.php';
 			});
 
-			$this->post('/new', function() {
-				
+			$this->post('/new', function () {
+
 				include 'NewListServer.php';
 			});
 
-			$this->get('/{id}:int', function($id) {
-				
+			$this->get('/{id}:int', function ($id) {
+
 				include 'ListIndividual.php';
 			});
 
-			$this->post('ajax/{id}:int', function($id) {
-				
+			$this->post('ajax/{id}:int', function ($id) {
+
 				include 'ListIndividualServer.php';
 			});
 
-			$this->get('/{id}:int/edit', function($id) {
+			$this->get('/{id}:int/edit', function ($id) {
 				include 'EditList.php';
 			});
 
-			$this->post('/{id}:int/edit', function($id) {
-				
+			$this->post('/{id}:int/edit', function ($id) {
+
 				include 'EditListServer.php';
 			});
 
-			$this->get('/{id}:int/delete', function($id) {
-				
+			$this->get('/{id}:int/delete', function ($id) {
+
 				include 'DeleteList.php';
 			});
 		});
 	}
 
 	if ($_SESSION['TENANT-' . app()->tenant->getId()]['AccessLevel'] == "Admin") {
-		$this->get('/sms', function() {
+		$this->get('/sms', function () {
 			$db = app()->db;
 			include 'SMSList.php';
 		});
 
-		$this->post('/sms/ajax', function() {
+		$this->post('/sms/ajax', function () {
 			$db = app()->db;
 			include 'SMSListFetch.php';
 		});
 	}
-
 }
