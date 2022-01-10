a36d2fcb93befcb749e14d507c5981a04d199dcf
Chris Heppell <clheppell@icloud.com>
2022-01-10 17:12:01 +0000

    Fix auth issue

--- src/controllers/admin-tools/scds-payments/router.php
+++ src/controllers/admin-tools/scds-payments/router.php
@@ -1,6 +1,6 @@
 <?php
 
-$this->get('/', function() {
+$this->get('/', function () {
   if (app()->user->hasPermission('SCDSPaymentsManager')) {
     include 'redirect.php';
   } else {
@@ -8,12 +8,20 @@ $this->get('/', function() {
   }
 });
 
-$this->group('/setup-direct-debit', function() {
-  $this->get('/', function() {
-    include 'direct-debit-setup.php';
+$this->group('/setup-direct-debit', function () {
+  $this->get('/', function () {
+    if (app()->user->hasPermission('SCDSPaymentsManager')) {
+      include 'direct-debit-setup.php';
+    } else {
+      include 'no-access.php';
+    }
   });
 
-  $this->get('/success', function() {
-    include 'direct-debit-setup-success.php';
+  $this->get('/success', function () {
+    if (app()->user->hasPermission('SCDSPaymentsManager')) {
+      include 'direct-debit-setup-success.php';
+    } else {
+      include 'no-access.php';
+    }
   });
 });
